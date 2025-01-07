946c123ee90abe29615c96733934a3c7abdc890f
louay24 <loueyzouaoui@takiacademyteam.com>
2025-01-07 23:29:35 +0100

    cors

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import AppDataSource from "./config/db";
 import sensorDataRoutes from "./routes/sensorData";
-
+import cors from "cors";
 dotenv.config();
 
 const app = express();
+app.use(
+  cors({
+    origin: "*", // allow all origins
+  })
+);
 app.use(express.json());
 app.use("/api/sensor-data", sensorDataRoutes);
 
